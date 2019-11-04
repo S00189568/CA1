5e0d9e8332523f20772a49063ba4ea3eca59ebf3
S00189568 <46888684+S00189568@users.noreply.github.com>
2019-11-04 12:39:29 +0000

    CA1

---
+++ CA1/CA1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CA1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Song s1 = new Song("ED SHEERAN", "BEAUTIFUL PEOPLE", 3.15, Genre.Pop);
+            Song s2 = new Song("REGARD", "RIDE IT", 3.37, Genre.Dance);
+            Song s3 = new Song("TONES & I", "DANCE MONKEY", 4.20, Genre.Dance);
+            Song s4 = new Song("POST MALONE", "CIRLES", 3.25, Genre.Pop);
+            Song s5 = new Song("ED SHEERAN", "SOUTH OF THE BORDER", 4.26, Genre.Other);
+
+
+            Song[] allSong = new Song[5];
+            allSong[0] = s1;
+            allSong[1] = s2;
+            allSong[2] = s3;
+            allSong[3] = s4;
+            allSong[4] = s5;
+
+            DisplaySong(allSong);
+
+            Console.WriteLine();
+            Array.Sort(allSong);
+
+            Console.WriteLine("Sorting the playlist by artist, then by song title :");
+
+        }//end of main method
+
+        static void DisplaySong(Song[] allSong)
+        {
+            foreach  (Song s in allSong)
+            {
+                Console.WriteLine(s);
+            }
+        }//end of allSong method
+    }
+}
---
+++ CA1/CA1/Song.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CA1
+{
+    enum Genre
+    {
+        Rock,
+        Pop,
+        Dance,
+        Other
+    }
+    class Song:IComparable 
+    {
+        //Properties
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public double Duration { get; set; }
+        public Genre genre { get; set; }
+
+        //Constructors
+        public Song(string title, string artist, double duration,Genre pop) : this(title, artist, duration)
+        {
+            Title = title;
+            Artist = artist;
+            Duration = duration;         
+        }
+
+        public Song(string title, string artist, double duration)
+        {
+            Title = title;
+            Artist = artist;
+            Duration = duration;
+        }
+
+        //Methods
+        public override string ToString()
+        {
+            return string.Format("Artist {0}  Song {1}   Duration {2}   Genre {3}",
+                Artist, Title, Duration, genre);
+        }
+
+        public int CompareTo(object obj)
+        {
+            Song that = (Song)obj;
+
+            return this.Artist.CompareTo(that.Artist);          
+        }
+    }
+}
